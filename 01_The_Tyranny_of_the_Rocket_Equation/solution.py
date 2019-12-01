dd71406b57467e895037ab956e94cc10384dea58
lpop <laur.pop.91@gmail.com>
2019-12-01 19:17:02 +0200

    final solution

--- 01_The_Tyranny_of_the_Rocket_Equation/solution.py
+++ 01_The_Tyranny_of_the_Rocket_Equation/solution.py
@@ -1,5 +1,9 @@
+import sys
+
+
 def get_fuel(mass):
     return int(mass / 3) - 2
+
 
 def get_additional_fuel(fuel):
     total = 0
@@ -10,14 +14,24 @@ def get_additional_fuel(fuel):
         total += fuel
     return total
 
+
 def get_total_fuel():
     total = 0
+    func_name = 'get_fuel'
     with open('input.txt', 'r') as f:
         for line in f:
             try:
                 num = int(line)
-                fuel = get_additional_fuel(num)
+                try:
+                    if sys.argv[1] == '2':
+                        func_name = 'get_additional_fuel'
+                except IndexError:
+                    pass
+                fuel = eval(f'{func_name}(num)')
                 total += fuel
             except ValueError:
                 print(f'{line} is not a number')
     return total
+
+
+print(get_total_fuel())
