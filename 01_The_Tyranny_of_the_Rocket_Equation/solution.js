b5db7e0b0bc9930919d8438e9588040695cc7ac4
lpop <laur.pop.91@gmail.com>
2019-12-05 22:03:15 +0100

    Optimised JS solution

--- 01_The_Tyranny_of_the_Rocket_Equation/solution.js
+++ 01_The_Tyranny_of_the_Rocket_Equation/solution.js
@@ -1,32 +1,23 @@
 const fs = require('fs')
 
-function get_fuel(mass) {
+const array = fs.readFileSync('input.txt').toString().split(
+    "\n").filter(Boolean).map(Number)
+
+function calculateTotalFuelNeeded(input) {
+    return input.reduce((total, moduleMass) => {
+        const funcName = process.argv[2] === '2' ? 'getFuelForMass' : 'getFuel'
+        const fuelNeeded = eval(`${funcName}(moduleMass)`)
+        return total + fuelNeeded
+    }, 0)
+}
+
+function getFuel(mass) {
     return parseInt(mass / 3) - 2
 }
 
-function get_additional_fuel(fuel) {
-    let total = 0
-    while (fuel) {
-        fuel = get_fuel(fuel)
-        if (fuel < 0) break;
-        total += fuel
-    }
-    return total
+function getFuelForMass(mass) {
+    const fuel = getFuel(mass)
+    return fuel < 0 ? 0 : fuel + getFuelForMass(fuel)
 }
 
-fs.readFile('input.txt', 'utf-8', function (err, file) {
-    if (err) throw err;
-
-    let total = 0
-    let func_name = 'get_fuel'
-    lines = file.split('\n').filter(Boolean)
-    for (let line of lines) {
-        num = parseInt(line)
-        if (process.argv[2] == '2') {
-            func_name = 'get_additional_fuel'
-        }
-        fuel = eval(`${func_name}(num)`)
-        total += fuel
-    }
-    console.log(total)
-});
+console.log(calculateTotalFuelNeeded(array))
