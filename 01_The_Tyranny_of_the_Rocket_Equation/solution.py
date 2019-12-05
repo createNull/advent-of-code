e0911f1685ec89202d052b7c47ce9d7339dbece5
lpop <laur.pop.91@gmail.com>
2019-12-05 23:09:30 +0100

    Optimised python solution

--- 01_The_Tyranny_of_the_Rocket_Equation/solution.py
+++ 01_The_Tyranny_of_the_Rocket_Equation/solution.py
@@ -1,37 +1,29 @@
-import sys
+from functools import reduce
 
 
-def get_fuel(mass):
+with open('input.txt') as f:
+    input_array = list(map(int, f.read().splitlines()))
+
+
+def required_fuel(mass):
     return int(mass / 3) - 2
 
 
-def get_additional_fuel(fuel):
-    total = 0
-    while fuel:
-        fuel = get_fuel(fuel)
-        if fuel < 0:
-            break
-        total += fuel
-    return total
+def mass_fuel(mass):
+    fuel = required_fuel(mass)
+    if fuel >= 0:
+        return fuel + mass_fuel(fuel)
+    return 0
 
 
-def get_total_fuel():
-    total = 0
-    func_name = 'get_fuel'
-    with open('input.txt', 'r') as f:
-        for line in f:
-            try:
-                num = int(line)
-                try:
-                    if sys.argv[1] == '2':
-                        func_name = 'get_additional_fuel'
-                except IndexError:
-                    pass
-                fuel = eval(f'{func_name}(num)')
-                total += fuel
-            except ValueError:
-                print(f'{line} is not a number')
-    return total
+def calc_total_fuel(array, option):
+    def accumulator(total, module_mass):
+        fuel_needed = required_fuel(module_mass)
+        if option == 2:
+            fuel_needed = mass_fuel(module_mass)
+        return total + fuel_needed
+    return reduce(accumulator, array, 0)
 
 
-print(get_total_fuel())
+print('Part 1:', calc_total_fuel(input_array, 1))
+print('Part 2:', calc_total_fuel(input_array, 2))
